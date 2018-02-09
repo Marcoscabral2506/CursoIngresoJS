b856701e6e4cc8a14d94d19e20e747ddea32843d
Marcoscabral2506 <36264957+Marcoscabral2506@users.noreply.github.com>
2018-02-09 12:26:19 -0300

    Parcial del 1 y 2
    parcial 1 completo, 2 haciendo

---
+++ dos.js
@@ -0,0 +1,14 @@
+function Mostrar()
+{
+  var importeProducto;
+  var importeFinal;
+  var resultado;
+
+  importeProducto= prompt(" ");
+  document.getElementById("importeFinal").value=importeProducto+21;
+
+  
+
+  
+
+}
