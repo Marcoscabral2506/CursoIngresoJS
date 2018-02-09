75a0f34798a1ab421b1af595e084173f006ad305
Marcoscabral2506 <36264957+Marcoscabral2506@users.noreply.github.com>
2018-02-09 12:06:39 -0300

    Ejercicio 7 al 10
    Descuento, aumento, parse int, numero entero

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,17 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+    var elDividiendo;
+    var elDivisor;
+    var resultado;
+
+    elDividiendo= document.getElementById("numeroDividendo").value;
+    elDivisor= document.getElementById("numeroDivisor").value;
+
+    elDividiendo= parseInt(elDividiendo);
+    elDivisor= parseInt(elDivisor);
+
+    resultado=elDividiendo%elDivisor;
+
+    alert("el resto es "+resultado);
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,17 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+     var sueldoUno;
+     var resultado;
+     var importe;
+         
+     sueldoUno= document.getElementById("sueldo").value;
+     resultado= document.getElementById("resultado").value;
 
-	
+     sueldoUno= parseInt(sueldoUno);
+     
+     importe= sueldoUno*1.1;
+
+     document.getElementById("resultado").value=importe;
+
 }
