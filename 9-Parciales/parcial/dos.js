563587ffc73eaa0d5ae06dc31924eb51c5d5560d
Marcoscabral2506 <36264957+Marcoscabral2506@users.noreply.github.com>
2018-02-10 12:09:26 -0300

    Parcial 1 ejercicio 2
    tomar dato por prompt. hacer el 21%, y mostrarlo por ID

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,14 @@
 function Mostrar()
 {
+  var importeProducto;
+  var importeFinal;
+  
+  importeProducto= prompt("ingrese importe ");
+
+  importeProducto=parseInt(importeProducto);
+  
+  importeFinal=importeProducto*1.21;
+
+  document.getElementById("importeFinal").value=importeFinal;
   
 }
