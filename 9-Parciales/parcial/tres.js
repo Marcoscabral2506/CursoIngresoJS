4110089e4ca0f0e68c42c4ac05767ecd83f2fe05
Marcoscabral2506 <36264957+Marcoscabral2506@users.noreply.github.com>
2018-02-10 13:02:16 -0300

    Parcial 1 ejercicio 3
    Perimetro x 3

--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,26 @@
 function Mostrar()
 {
+  var largo;
+  var ancho;
+  var perimetro;
+  var perimetroDos;
+
+  largo=document.getElementById("alrgo").value;
+
+  largo=parseInt(largo);
+  
+  ancho=document.getElementById("ancho").value;
+
+  ancho=parseInt(ancho);
+
+  perimetro=largo+ancho+largo+ancho;
+
+  perimetroDos=perimetro*3;
+
+  document.getElementById("alrgo").value=largo;
+  
+  document.getElementById("ancho").value=ancho;
+  
+  alert("Usted necesita "+perimetroDos+" metros de alambre");
 
 }
