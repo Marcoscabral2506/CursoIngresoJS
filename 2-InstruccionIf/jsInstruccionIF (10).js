0bc9b09e10e46828645df1ac849e202895f73662
Marcoscabral2506 <36264957+Marcoscabral2506@users.noreply.github.com>
2018-02-15 12:20:38 -0300

    instruccion 8 9 10

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,52 @@
 function Mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	
-
+	var nota;
+	nota=parseInt((Math.random()*(11-1))+1)
+	alert(nota);
+	if(nota>=9)
+   {
+	   alert("excelente");
+   }
+	else if (nota<=8 && nota>=6)
+	{ 
+	 alert("muy bien");
+    }
+	else if (nota<=5 && nota>=4)
+	{
+		alert("bien");
+	}
+	else if (nota<=3 && nota>=2)
+	{
+		alert("mal");
+	}
+	else if (nota<=1 && nota>=0)
+	{
+		alert("desastre");
+	}
 }//FIN DE LA FUNCIÓN
+/* else if (nota>=6) esta bien porque si es mayor que 8 entra en el primero
+else if (nota>=4)
+else if (nota>=2)
+else 
+
+o se puede hacer
+else
+{ 
+	if(nota>=6)
+	{
+
+ 	} 
+	else
+	{
+
+	}
+		if(nota>=4)
+		{
+
+		}
+		else
+		{
+			if(nota>=4)
+		
+		}
+} 
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,5 +2,10 @@ function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+	var numero;
+	numero=parseInt((Math.random()*(11-1))+1)
+	alert(numero);
 
-}//FIN DE LA FUNCIÓN
+}//FIN DE LA FUNCIÓN  [min, max]
+//Math random para que ponga un numero random
+//Math random con M mayuscula o no anda.
