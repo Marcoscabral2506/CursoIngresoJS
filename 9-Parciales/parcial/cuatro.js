92fa35942e881804320aceaa9191f7d2f5e89818
Marcoscabral2506 <36264957+Marcoscabral2506@users.noreply.github.com>
2018-02-15 11:32:43 -0300

    Parcial 1 ejercicio 4
    Else if para que no sea tan largo
    mostrar resultado en document.write
    resultado o multiplicar o sumar o restar

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,28 @@
 function Mostrar()
 {
+  var numeroUno;
+  var numeroDos;
+  var resultado;
 
+  numeroUno=prompt("Ingrese numero");
+  numeroDos=prompt("Ingrese numero ");
+
+  numeroUno=parseInt(numeroUno);
+  numeroDos=parseInt(numeroDos);
+
+  if(numeroUno==numeroDos)
+  {
+      resultado=numeroUno*numeroDos;
+  }
+ else if (numeroUno>numeroDos)
+ {
+     resultado=numeroUno-numeroDos;
+ }
+ else
+ {
+     resultado=numeroUno+numeroDos;
+ }
+ document.write(resultado);
 }
+
+/* else if para evitar hacer mas largo, mas entendible, se concatena al if
