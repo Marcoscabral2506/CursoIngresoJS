2b72a9df10b868062cf269afe822841dcb32e296
Marcoscabral2506 <36264957+Marcoscabral2506@users.noreply.github.com>
2018-02-22 21:12:24 -0300

    switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,11 +1,20 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+    var mesDelAño;
+    mesDelAño= document.getElementById('mes').value;
+    
+    switch(mesDelAño)
+    {
+           
+        case "Febrero":
+            alert("Este mes tiene menos de 29 días");
+            break; 
+        default:
+            alert("Este mes tiene mas de 30 días o más");
+            break;
 
+    }
 //alert (mesDelAño);
-	
-	
-
 
 }//FIN DE LA FUNCIÓN
