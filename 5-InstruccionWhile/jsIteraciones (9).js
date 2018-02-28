d7d4930e9256738faf65dbca868c5f8dff593cae
Marcoscabral2506 <36264957+Marcoscabral2506@users.noreply.github.com>
2018-02-28 18:11:40 -0300

    9 while
    minimo y maximo

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -2,17 +2,39 @@ function Mostrar()
 {
 
 	var contador=0;
+	var numero;
+	var maximo;
+	var minimo;
 	// declarar variables
 	
 	var respuesta='si';
 
 	while(respuesta!='no')
 	{
-		
-	
+		contador= contador+1;
+		numero= prompt("Ingrese numero");
+		numero=parseInt(numero);
+
+		if( contador== 1)
+		{
+			maximo=numero;
+			minimo= numero;
+		}
+		else{
+			if(numero>maximo)
+			{
+				maximo=numero;
+			}
+		if (numero<minimo)
+			{	
+				minimo=numero;
+			}
+		}
+	respuesta=prompt("Si desea continuar ponga si, de lo contrario ponga no");
 	}
 
-
+	document.getElementById('maximo').value=maximo;
+	document.getElementById('minimo').value=minimo;
 
 
 }//FIN DE LA FUNCIÓN
