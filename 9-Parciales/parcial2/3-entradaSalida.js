9b2113e93382b3323d27b17f47ccb41a357defed
Marcoscabral2506 <36264957+Marcoscabral2506@users.noreply.github.com>
2018-03-01 12:17:47 -0300

    Add files via upload

--- 9-Parciales/parcial2/3-entradaSalida.js
+++ 9-Parciales/parcial2/3-entradaSalida.js
@@ -1,7 +1,21 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 3-EntradaSalida");
+	var largo;
+	var ancho;
+	var perimetro;
+	var resultado;
+
+	ancho=document.getElementById("ancho").value;
+	largo=document.getElementById("largo").value;
+
+	ancho=parseInt(ancho);
+	largo=parseInt(largo);
+
+	perimetro=ancho*2+largo*2;
+
+	resultado=perimetro*6;
 	
+	alert("Ustede necesita "+resultado + " metros de alambre");
 }
 
--- 9-Parciales/parcial2/6-iteraciones.js
+++ 9-Parciales/parcial2/6-iteraciones.js
@@ -1,7 +1,33 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 6-iteraciones");
+	var importe=-1, maximo, minimo, diaMax, diaMin;
+	var contador=0;
+	var acumulador;
 	
+
+	while(contador<7)
+	{
+		while(importe<0){
+		importe=prompt("ingrese importe");
+		importe=parseInt(importe);
+		}
+		if(contador==1)
+		{
+			maximo=importe;
+			minimo=importe;
+		}
+		else{
+			if(importe>maximo)
+			{
+				maximo=importe;
+			}
+		if(importe<minimo){
+				minimo=importe;
+			}
+		}
+
+	}		
+alert("El mayor importe fue "+maximo+ " y su menor importe fue "+minimo);
 }
 
